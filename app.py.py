6356b7f1ec0fdac0e5f2a77d0bfe7d9ae8ea2863
Silence Gweshe <silencegweshe8@gmail.com>
2025-04-20 19:15:09 +0100

    Initial commit

---
+++ app.py.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import sqlite3
+import threading
+
+app = Flask(__name__)
+
+@app.route("/users")
+def get_users():
+    conn = sqlite3.connect("SQLite&Python.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    conn.close()
+
+    users = [{"id": row[0], "name": row[1], "email": row[2]} for row in rows]
+    return jsonify(users)
+
